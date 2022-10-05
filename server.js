dc14303a85955410e36f0e16fdd260691b7ac258
RashadXalil <rashadmkh@code.edu.az>
2022-10-05 09:29:55 +0400

    UserSchema ready

--- server.js
+++ server.js
@@ -1,7 +1,43 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
+const UserModel = require('./Models/Users')
 const app = express()
-
+async function mongooseConnect() {
+  await mongoose.connect(
+    'mongodb+srv://rashadkhll:rashad918X.@cluster0.fmxu4.mongodb.net/?retryWrites=true&w=majority',
+  )
+}
+mongooseConnect()
+app.use(express.json())
+//add
+app.post('/users', async (req, res) => {
+  const newUser = new UserModel(req.body)
+  await newUser.save()
+  res.status(201).send('User Created !')
+})
+//get all
+app.get('/users', async (req, res) => {
+  const prods = await UserModel.find()
+  res.json(prods)
+})
+// get by id
+app.get('/users/:id', async (req, res) => {
+  const id = req.params.id
+  const user = await UserModel.findById(id)
+  res.status(200).json(user)
+})
+//update
+app.patch('/users/:id', async (req, res) => {
+  const id = req.params.id
+  const user = await UserModel.findByIdAndUpdate(id, req.body)
+  res.status(200).json(user)
+})
+//delete
+app.delete('/users/:id', async (req, res) => {
+  const id = req.params.id
+  await UserModel.findByIdAndDelete(id)
+  res.status(200).send('Deleted !')
+})
 app.listen(8080, () => {
-  console.log('Server Running on 8080')
+  console.log('server running on 8080')
 })
